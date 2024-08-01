8ef893e4e34ad8f6a0b8d9a02758932089a09ff2
Renan Cavichi <renancavichi@gmail.com>
2024-07-31 22:15:54 -0300

    add import type module and hello world express

--- README.md
+++ README.md
@@ -6,6 +6,11 @@
 node -v #verifica versão do node
 npm -v #verifica versão do npm
 npm init #cria o package json
+npm i #instala todas as dependências listadas no package.json
+npm i nome-do-pacote #instala pacotes
+npm uninstall #desinstala os pacotes
+npm run nome-do-script #executa um script do package.json
+npm start #executa o script start do package.json
 ```
 
 ### Git
--- src/server.js
+++ src/server.js
@@ -1,1 +1,16 @@
-console.log('Olá Mundo!')
+//const express = require('express')
+import express from 'express'
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Olá Mundo!')
+})
+
+app.get('/user', (req, res) => {
+    res.json({nome: "renan", email: "renancavichi@gmail.com" })
+})
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`)
+})
