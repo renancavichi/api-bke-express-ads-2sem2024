426f4d68b7450db6f92f108b6541d921a4425e0f
Renan Cavichi <renancavichi@gmail.com>
2024-08-21 21:24:54 -0300

    use user model with prisma

--- src/controllers/user/userList.js
+++ src/controllers/user/userList.js
@@ -1,8 +1,8 @@
-//import { getAll } from '../../models/userModel.js'
+import { getAll } from '../../models/userModel.js'
 
 const userList = async (req, res) => {
 
-    //const users = await getAll()
+    const users = await getAll()
     res.json(users)
 }
 
