ffccb6b4438ff7b25ca13053b2d59f2d864f11f9
Renan Cavichi <renancavichi@gmail.com>
2024-10-30 21:21:31 -0300

    add uuid for public_id and bcrypt to pass

--- src/controllers/user/createUser.js
+++ src/controllers/user/createUser.js
@@ -1,4 +1,6 @@
 import { create, validateUserToCreate } from "../../models/userModel.js"
+import { v4 as uuid } from 'uuid'
+import bcrypt from 'bcrypt'
 
 const createUser = async (req, res, next) => {
    try{
@@ -11,6 +13,9 @@ const createUser = async (req, res, next) => {
                     fieldErrors: userValidated.error.flatten().fieldErrors
             })
         }
+
+        userValidated.data.public_id = uuid()
+        userValidated.data.pass = bcrypt.hashSync(userValidated.data.pass, 10)
 
         const result = await create(userValidated.data) 
 
